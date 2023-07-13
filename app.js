3523d81da678f8a57faea8083c3761393317fb64
Dharsan <dharsansrinivasan2004@gmail.com>
2023-07-13 13:32:36 +0530

    added redis for caching

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 
+const redis = require("redis");
+const redisClient = redis.createClient(6379);
+
+const shortUrlModel = require("./models/shortUrl");
+
 require("dotenv").config();
 
 const shortUrl = require("./models/shortUrl");
@@ -17,16 +22,16 @@ mongoose.connect(mongoURL, {
 
 function generateShortURL(longURL) {
     const hash = crypto.createHash("sha512").update(longURL).digest("hex");
-    const shortURL = hash.substring(0, 7); // Extract a portion of the hash for the shortened URL
+    const shortURL = hash.substring(0, 7);
     return shortURL;
 }
 
-// const longURL = "https://example.com/very/long/url";
-// const shortenedURL = generateShortURL(longURL);
-// console.log(shortenedURL);
-
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
+
+redisClient.on("error", (err) => {
+    console.error("Redis connection error:", err);
+});
 
 app.get("/", async (req, res) => {
     const shorturls = await shortUrl.find();
@@ -34,14 +39,13 @@ app.get("/", async (req, res) => {
 });
 
 app.post("/shortUrls", async (req, res) => {
-    console.log(req.body.fullurl.length);
     const longURL = req.body.fullurl;
     const shortenedURL = generateShortURL(longURL);
-    console.log(shortenedURL);
 
     if (req.body.fullurl.length === 0) {
         return res.redirect("/");
     }
+
     await shortUrl.create({
         full: req.body.fullurl,
         short: shortenedURL,
@@ -50,23 +54,46 @@ app.post("/shortUrls", async (req, res) => {
 });
 
 app.get("/cleardb", async (req, res) => {
-    console.log("cleared");
     await shortUrl.deleteOne();
     res.redirect("/");
 });
 
 app.get("/:shortUrl", async (req, res) => {
-    console.log(req.params.shortUrl);
-
-    const short_id = await shortUrl.findOne({ short: req.params.shortUrl });
-    console.log(short_id);
-    if (short_id == null) return res.sendStatus(404);
-
-    short_id.clicks++;
-    await short_id.save();
-    res.redirect(short_id.full);
+    const shortURL = req.params.shortUrl;
+    const url = await shortUrl.findOne({ short: shortURL });
+    url.clicks++;
+    await url.save();
+    try {
+        if (redisClient.connected) {
+            redisClient.get(shortURL, async (err, response) => {
+                if (response) {
+                    console.log("URL successfully retrieved from cache");
+                    const cachedUrlData = JSON.parse(response);
+                    const cachedUrl = new shortUrlModel(cachedUrlData); // Create a new instance of the model
+                    // cachedUrl.clicks++;
+                    // await cachedUrl.save();
+                    res.status(200).redirect(cachedUrl.full);
+                } else {
+                    const url = await shortUrl.findOne({ short: shortURL });
+                    if (url) {
+                        redisClient.setex(shortURL, 600, JSON.stringify(url));
+                        console.log(
+                            "URL successfully retrieved from the database"
+                        );
+                        res.redirect(url.full);
+                    } else {
+                        res.sendStatus(404);
+                    }
+                }
+            });
+        } else {
+            throw new Error("Redis client connection closed");
+        }
+    } catch (err) {
+        res.status(500).send({ error: err.message });
+    }
 });
 
 app.listen(3000, () => {
-    console.log("server is Running");
+    console.log("Server is running on port 3000");
 });
